39ad1e72e1db464729e8448ff8039c71e3236553
Sam Hertz <samhertz@vt.edu>
2022-02-12 10:13:45 -0800

    Initial Commit

---
+++ City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Samuel Hertz on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights App
+//
+//  Created by Samuel Hertz on 2/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
